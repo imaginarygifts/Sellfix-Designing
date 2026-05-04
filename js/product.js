6170f3bb727162046016ad407ae733e429a4d824
Imaginary Gifts <imaginarygifts.in@gmail.com>
2026-05-05 02:19:00 +0530

    Update product.js

--- js/product.js
+++ js/product.js
@@ -205,36 +205,14 @@ let html = `
 <div class="product-header">
   ${badgeHTML}
   <h2>${product.name}</h2>
-  ${priceHTML}
+
   <p>${product.description}</p>
 
+  ${priceHTML}
 </div>
 `;
 
-  // ===== RELATED DESIGNS =====
-  if (relatedProducts.length > 1) {
-    html += `
-      <div class="design-wrap">
-        <h4>Select Design</h4>
-        <div class="design-row">
-    `;
-
-    relatedProducts.forEach(p => {
-      const active = p.name === product.name ? "active" : "";
-      html += `
-        <div class="design-card ${active}" onclick="goToDesign('${p.id}')">
-          <img src="${p.images?.[0] || ""}">
-          <small>${p.name}</small>
-          <div class="price">
-  ₹${(p.salePrice && p.salePrice < p.basePrice) ? p.salePrice : p.basePrice}
-</div>
-        </div>
-      `;
-    });
-
-    html += `</div></div>`;
-  }
-
+ 
   // COLORS
   if (product.variants?.colors?.length) {
     html += `<h4>Colors</h4><div class="variant-row">`;
@@ -293,6 +271,31 @@ let html = `
     });
   }
 
+
+ // RELATED DESIGN
+
+if (relatedProducts.length > 1) {
+  html += `
+    <div class="design-wrap">
+      <h3>You may also like</h3>
+      <div class="design-row">
+  `;
+
+  relatedProducts.forEach(p => {
+    const active = p.name === product.name ? "active" : "";
+    html += `
+      <div class="design-card ${active}" onclick="goToDesign('${p.id}')">
+        <img src="${p.images?.[0] || ""}">
+        <small>${p.name}</small>
+        <div class="price">
+₹${(p.salePrice && p.salePrice < p.basePrice) ? p.salePrice : p.basePrice}
+</div>
+      </div>
+    `;
+  });
+
+  html += `</div></div>`;
+}
   details.innerHTML = html;
 }
 
